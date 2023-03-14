fb45263c804d3ce2fed15c7bce3d03fb329a5988
Lucy Brandão <lucyjbrandao@gmail.com>
2023-03-14 12:17:20 -0300

    fix: adiciona build

---
+++ build/en/assets/js/6a3112a0.71063f5c.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkhomepage=self.webpackChunkhomepage||[]).push([[5387],{5641:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome","permalink":"/en/blog/welcome"},{"title":"Hello","permalink":"/en/blog/hello-world"},{"title":"Hola","permalink":"/en/blog/hola"}]}')}}]);
